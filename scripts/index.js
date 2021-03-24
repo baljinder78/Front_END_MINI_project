120600916ff718f82dd890e6335c87d86f2cd918
Baljinder Singh <baljinder9219@gmail.com>
2021-03-24 15:23:15 +0530

    DONE

--- scripts/index.js
+++ scripts/index.js
@@ -20,18 +20,28 @@ function ADDuser() {
         var tr = document.createElement("TR");
 
         var td = document.createElement("TD");
+        var td_p_1 = document.createElement("P");
         var inp_1 = document.createTextNode(Name);
-        td.appendChild(inp_1);
+        td_p_1.appendChild(inp_1);
+        td_p_1.setAttribute("class", "imp");
+        td.appendChild(td_p_1);
+
         tr.appendChild(td);
 
         var td_2 = document.createElement("TD");
+        var td_p_2 = document.createElement("P");
         var inp_2 = document.createTextNode(mob_no);
-        td_2.appendChild(inp_2);
+        td_p_2.appendChild(inp_2);
+        td_p_2.setAttribute("class", "imp");
+        td_2.appendChild(td_p_2);
         tr.appendChild(td_2);
 
         var td_3 = document.createElement("TD");
+        var td_p_3 = document.createElement("P");
         var inp_3 = document.createTextNode(email);
-        td_3.appendChild(inp_3);
+        td_p_3.appendChild(inp_3);
+        td_p_3.setAttribute("class", "imp");
+        td_3.appendChild(td_p_3);
         tr.appendChild(td_3);
 
         var td_4 = document.createElement("TD");
@@ -39,6 +49,7 @@ function ADDuser() {
         var text_btn = document.createTextNode("Delete")
         delet.appendChild(text_btn);
         delet.setAttribute("onclick", "delete_user()");
+        delet.setAttribute("class", "delete_btn");
         td_4.appendChild(delet);
 
         tr.appendChild(td_4);
